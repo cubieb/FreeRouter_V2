935917d6c47847b6e55f949f22660740efa80110
bobcafer <bobcafer@live.cn>
2014-10-23 13:20:20 +0800

    Update vpnup.sh
    add dns query route to vpn table.

--- OpenWRT/etc/ppp/ip-up.d/vpnup.sh
+++ OpenWRT/etc/ppp/ip-up.d/vpnup.sh
@@ -4,6 +4,8 @@ echo "VPN UP @$(date +"%T@%Y-%m-%d")" >>$LOG
 VPN_DEV=$(ifconfig | grep "pptp" | sed -e "s#^\([^ ]*\) .*#\1#g")
 echo "Add VPN device for table vpn @$(date +"%T@%Y-%m-%d")" >>$LOG
 ip route add default dev $VPN_DEV table vpn 
+ip route add 8.8.8.8 dev $VPN_DEV table vpn
+ip route add 8.8.4.4 dev $VPN_DEV table vpn
 echo "Add IPs marked by firewall to table vpn @$(date +"%T@%Y-%m-%d")" >>$LOG  
 ip rule add fwmark 1 priority 1984 table vpn
 /etc/init.d/dnsmasq restart
